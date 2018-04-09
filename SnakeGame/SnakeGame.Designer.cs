38a01916038bbfc7538ed12e59456a45afe7dbb6
NaibRT <naibrt@code.edu.az>
2018-04-09 07:40:34 +0400

    snake game

--- SnakeGame/SnakeGame.Designer.cs
+++ SnakeGame/SnakeGame.Designer.cs
@@ -28,44 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(453, 38);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(39, 20);
-            this.textBox1.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.YellowGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(12, 527);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
             // 
-            // textBox2
+            // label2
             // 
-            this.textBox2.Location = new System.Drawing.Point(453, 12);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(39, 20);
-            this.textBox2.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.YellowGreen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(74, 527);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(-2, 510);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(523, 10);
+            this.label3.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(514, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(481, 529);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chocolate;
+            this.button1.Location = new System.Drawing.Point(208, 529);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(454, 529);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "0";
             // 
             // SnakeGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(504, 501);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.ClientSize = new System.Drawing.Size(514, 561);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
             this.KeyPreview = true;
             this.Name = "SnakeGame";
             this.Text = "Form2";
-            this.Load += new System.EventHandler(this.SnakeGame_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
     }
 }
--- SnakeGame/SnakeGame.cs
+++ SnakeGame/SnakeGame.cs
@@ -7,102 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace SnakeGame
 {
     public partial class SnakeGame : Form
     {
-        public int top = 0;
+        public int top = 30;
         public int left = 0;
         public int topbody = 10;
         public PictureBox snake;
-        //public Button snakebas;// = new Button();
         public PictureBox meal;
         public Random teq = new Random();
         public List<PictureBox> body = new List<PictureBox>();
         public int count = 0;
         public int price = 0;
-
+        public Timer time;
+        
         public SnakeGame()
         {
+      
             InitializeComponent();
-            Random teq = new Random();
-            meal = new PictureBox();
-            meal.Enabled = false;
-            meal.Left = 0;
-            meal.Top = 100;
-            meal.Height = 10;
-            meal.Width = 10;
-            meal.BackColor = Color.Red;
-
-
-
-
-
-
-            for (int i = 0; i < 3; i++)
-            {
-                snake = new PictureBox();
-                snake.Height = 10;
-                snake.Width = 10;
-                //snake.FlatStyle = FlatStyle.Popup;
-                snake.Top = top;
-                snake.Left = left;
-                
-                snake.BackColor = Color.White;
-                body.Add(snake);
-
-                top += 10;
-            }
-                //left += 20;
-            
-          
-
-            foreach (var item in body)
-            {
-                Controls.Add(item);
-            }
-            Controls.Add(meal);
-            
-            this.Width = 520;
-            this.Height = 540;
+            snakemake();
+            this.Width = 530;
+            this.Height = 600;
             this.KeyUp += new KeyEventHandler(SnakeGame_KeyUp);
-
-
+            timer1.Interval = 1000;
             void SnakeGame_KeyUp(object sender, KeyEventArgs e)
             {
                 if (e.KeyCode == Keys.Up)
                 {
-                    for (int i = 0; i <body.Count-1 ; i++)
-                    {
-                        if (body[i].Top < 0)
+                        for (int i = 0; i < body.Count - 1; i++)
                         {
-                            top = 500;
-                            body[i].Top = top;
-                            //top -= 20;
-                            body[i].Top = body[i + 1].Top;
-                            body[i].Left = body[i + 1].Left;
+                            if (body[i].Top < 24)
+                            {
+                                top = 500;
+                                body[i].Top = top;
+                                //top -= 20;
+                                body[i].Top = body[i + 1].Top;
+                                body[i].Left = body[i + 1].Left;
 
+                            }
+                            else
+                            {
+                                body[i].Top = body[i + 1].Top;
+                                body[i].Left = body[i + 1].Left;
+                            }
+                        }
+                        if (body[body.Count - 1].Top < 24)
+                        {
+                            body[body.Count - 1].Top = 500;
+                            body[body.Count - 1].Top -= 10;
                         }
                         else
                         {
-                            body[i].Top = body[i + 1].Top;
-                            body[i].Left = body[i + 1].Left;
+                            body[body.Count - 1].Top -= 10;
                         }
-                        
-                    }
-                    if (body[body.Count - 1].Top < 0)
-                    {
-                        body[body.Count - 1].Top = 500;
-                        body[body.Count - 1].Top -= 10;
-                    }
-                    else
-                    {
-                        body[body.Count - 1].Top -= 10;
-                    }
-                    
-                    mealeating();
-
+                    gameover();
+                     mealeating();
                 }
                 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\
                 if (e.KeyCode == Keys.Down)
@@ -118,7 +80,7 @@ namespace SnakeGame
                             //top += 20;
                             //snakebas.Top = top;
 
-                            top = 0;
+                            top = 24;
                             body[i].Top =top;
                             body[i].Top = body[i + 1].Top;
                             body[i].Left = body[i + 1].Left;
@@ -135,13 +97,14 @@ namespace SnakeGame
                     }
                     if (body[body.Count - 1].Top >500)
                     {
-                        body[body.Count - 1].Top = 0;
+                        body[body.Count - 1].Top = 24;
                         body[body.Count - 1].Top += 10;
                     }
                     else
                     {
                         body[body.Count - 1].Top += 10;
                     }
+                    gameover();
                     mealeating();
                 }
 
@@ -153,7 +116,7 @@ namespace SnakeGame
                         if (body[i].Left > 500)
                         {
                             //left = 0;
-                            body[i].Left =0;
+                            body[i].Left = 24;
                             body[i].Top = body[i + 1].Top;
                             body[i].Left = body[i + 1].Left;
                         }
@@ -166,13 +129,14 @@ namespace SnakeGame
                     }
                     if (body[body.Count - 1].Left > 500)
                     {
-                        body[body.Count - 1].Left = 0;
+                        body[body.Count - 1].Left = 24;
                         body[body.Count - 1].Left += 10;
                     }
                     else
                     {
                         body[body.Count - 1].Left += 10;
                     }
+                    gameover();
                      mealeating();
                 }
                 //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -180,7 +144,7 @@ namespace SnakeGame
                 {
                     for (int i = 0; i < body.Count-1; i++)
                     {
-                        if (body[i].Left < 0)
+                        if (body[i].Left < 24)
                         {
                             left = 500;
                             body[i].Left = left;
@@ -194,7 +158,7 @@ namespace SnakeGame
                         }
 
                     }
-                    if (body[body.Count - 1].Left < 0)
+                    if (body[body.Count - 1].Left < 24)
                     {
                         body[body.Count - 1].Left = 500;
                         body[body.Count - 1].Left -= 10;
@@ -204,26 +168,25 @@ namespace SnakeGame
                         body[body.Count - 1].Left -= 10;
                     }
 
-                
+                    gameover();
                     mealeating();
 
                 }
             }
-
+          
         }
           public  void mealeating()
         {
             if (body[body.Count - 1].Location.X == meal.Location.X && body[body.Count - 1].Location.Y == meal.Location.Y)
                 {
-
+                song();
                 Controls.Remove(meal);
-                body[body.Count-1].BackColor = Color.Green;
-                textBox1.Text = "";
-                textBox2.Text = "";
+                
+             
                 count++;
                 price += 5;
-                textBox1.Text = count.ToString();
-                textBox2.Text = price.ToString();
+                label1.Text = count.ToString();
+                label2.Text = price.ToString();
                 makemeal();
                 snakegrow();
             }
@@ -231,11 +194,12 @@ namespace SnakeGame
         }
         public void makemeal()
         {
+            Random teq = new Random();
             //int x = 0;
             while (true)
             {
-                int x = teq.Next(0, 500);
-                int y = teq.Next(0, 500);
+                int x = teq.Next(30, 500);
+                int y = teq.Next(30, 500);
                 if (x % 10 == 0 && y % 10 == 0)
                 {
                     meal = new PictureBox();
@@ -257,7 +221,7 @@ namespace SnakeGame
             //meal.FlatStyle = FlatStyle.Flat;
             Controls.Add(meal);
             
-        }//makemeal de deyisiklikler olunmalidi.
+        }
         public void snakegrow()
         {
             snake = new PictureBox();
@@ -266,16 +230,91 @@ namespace SnakeGame
             snake.Width = 10;
             //snake.FlatStyle = FlatStyle.Popup;
           
-           snake.Top = body[0].Top + 10;
-            snake.Left = body[0].Left;
-            snake.BackColor = Color.White;
+           snake.Top = body[body.Count-1].Top;
+            snake.Left = body[body.Count - 1].Left;
+            snake.BackColor = Color.Black;
             Controls.Add(snake);
             body.Add(snake);
         }
-
-        private void SnakeGame_Load(object sender, EventArgs e)
+        public void gameover()
         {
+            for (int i = 0; i < body.Count - 1; i++)
+            {
+                if (body[body.Count - 1].Top == body[i].Top && body[body.Count - 1].Left == body[i].Left)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Game Over");
+                   // restart();
+                    
+                }
+            }
+        }
+        public void snakemake()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                snake = new PictureBox();
+                snake.Height = 10;
+                snake.Width = 10;
+                //snake.FlatStyle = FlatStyle.Popup;
+                snake.Top = top;
+                snake.Left = left;
 
+                snake.BackColor = Color.Black;
+                body.Add(snake);
+
+                top += 10;
+            }
+            //left += 20;
+        foreach (var item in body)
+            {
+                Controls.Add(item);
+            }
+            meal = new PictureBox();
+            meal.Enabled = false;
+            meal.Left = 0;
+            meal.Top = 100;
+            meal.Height = 10;
+            meal.Width = 10;
+            meal.BackColor = Color.Red;
+            Controls.Add(meal);
+
+        }
+        public void restart()
+        {
+            Controls.Clear();
+            Controls.Clear();
+            body.Clear();
+  
+
+        }
+        public void song()
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\Users\Naib\Downloads\1.wav");
+            player.Play();
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            int secound = Convert.ToInt32(label4.Text);
+            int minut = Convert.ToInt32(label5.Text);
+            secound++;
+            label4.Text = secound.ToString();
+            if (secound==60)
+            {
+                minut++;
+                secound = 0;
+                label4.Text = secound.ToString();
+            }
+            label5.Text = minut.ToString();
         }
     }
 }
